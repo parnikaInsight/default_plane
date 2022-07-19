e3c1689d28d5dc34d8744276eeda3bdcb24075e0
parnikasaxena <paxsethorld@gmail.com>
2022-07-19 13:41:40 -0500

    bevy_blender code

--- src/main.rs
+++ src/main.rs
@@ -1,236 +1,70 @@
 use bevy::prelude::*;
-// use bevy_blender::*;
+use bevy_blender::*;
 
-// // Use pan orbit camera
-// mod camera;
+// Use pan orbit camera
+mod camera;
 
-// fn main() {
-//     App::new()
-//         .add_plugins(DefaultPlugins)
-//         .add_plugin(BlenderPlugin)
-//         .add_startup_system(setup)
-//         .add_system(camera::pan_orbit_camera)
-//         .run();
-// }
-
-// fn setup(mut commands: Commands, asset_server: ResMut<AssetServer>) {
-//     spawn_blender_object(
-//         &mut commands,
-//         &asset_server,
-//         "robot.blend",
-//         "Cube.002",
-//         false,
-//         None,
-//     );
-
-//     // Light and camera
-//     commands.spawn_bundle(PointLightBundle {
-//         transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
-//         ..Default::default()
-//     });
-
-//     let translation = Vec3::new(5.0, 5.0, 5.0);
-//     let radius = translation.length();
-
-//     commands
-//         .spawn_bundle(PerspectiveCameraBundle {
-//             transform: Transform::from_translation(translation).looking_at(Vec3::ZERO, Vec3::Y),
-//             ..Default::default()
-//         })
-//         .insert(camera::PanOrbitCamera {
-//             radius,
-//             ..Default::default()
-//         });
-// }
-
-
-
-// use bevy::prelude::*;
-
-// Loads and renders a glTF file as a scene.
-
-// // fox example
-// fn main() {
-//     App::new()
-//         .add_plugins(DefaultPlugins)
-//         .insert_resource(AmbientLight {
-//             color: Color::WHITE,
-//             brightness: 1.0,
-//         })
-//         .add_startup_system(setup)
-//         .add_system(setup_scene_once_loaded)
-//         .add_system(keyboard_animation_control)
-//         .run();
-// }
-
-// struct Animations(Vec<Handle<AnimationClip>>);
-
-// fn setup(
-//     mut commands: Commands,
-//     asset_server: Res<AssetServer>,
-//     mut meshes: ResMut<Assets<Mesh>>,
-//     mut materials: ResMut<Assets<StandardMaterial>>,
-// ) {
-//     // Insert a resource with the current scene information
-//     commands.insert_resource(Animations(vec![
-//         asset_server.load("models/animated/Fox.glb#Animation2"),
-//         asset_server.load("models/animated/Fox.glb#Animation1"),
-//         asset_server.load("models/animated/Fox.glb#Animation0"),
-//     ]));
-
-//     // Camera
-//     commands.spawn_bundle(Camera3dBundle {
-//         transform: Transform::from_xyz(100.0, 100.0, 150.0)
-//             .looking_at(Vec3::new(0.0, 20.0, 0.0), Vec3::Y),
-//         ..default()
-//     });
-
-//     // Plane
-//     commands.spawn_bundle(PbrBundle {
-//         mesh: meshes.add(Mesh::from(shape::Plane { size: 500000.0 })),
-//         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-//         ..default()
-//     });
-
-//     // Light
-//     commands.spawn_bundle(DirectionalLightBundle {
-//         transform: Transform::from_rotation(Quat::from_euler(
-//             EulerRot::ZYX,
-//             0.0,
-//             1.0,
-//             -std::f32::consts::FRAC_PI_4,
-//         )),
-//         directional_light: DirectionalLight {
-//             shadows_enabled: true,
-//             ..default()
-//         },
-//         ..default()
-//     });
-
-//     // Fox
-//     commands.spawn_bundle(SceneBundle {
-//         scene: asset_server.load("models/animated/Fox.glb#Scene0"),
-//         ..default()
-//     });
-
-//     println!("Animation controls:");
-//     println!("  - spacebar: play / pause");
-//     println!("  - arrow up / down: speed up / slow down animation playback");
-//     println!("  - arrow left / right: seek backward / forward");
-//     println!("  - return: change animation");
-// }
-
-// // Once the scene is loaded, start the animation
-// fn setup_scene_once_loaded(
-//     animations: Res<Animations>,
-//     mut player: Query<&mut AnimationPlayer>,
-//     mut done: Local<bool>,
-// ) {
-//     if !*done {
-//         if let Ok(mut player) = player.get_single_mut() {
-//             player.play(animations.0[0].clone_weak()).repeat();
-//             *done = true;
-//         }
-//     }
-// }
-
-// fn keyboard_animation_control(
-//     keyboard_input: Res<Input<KeyCode>>,
-//     mut animation_player: Query<&mut AnimationPlayer>,
-//     animations: Res<Animations>,
-//     mut current_animation: Local<usize>,
-// ) {
-//     if let Ok(mut player) = animation_player.get_single_mut() {
-//         if keyboard_input.just_pressed(KeyCode::Space) {
-//             if player.is_paused() {
-//                 player.resume();
-//             } else {
-//                 player.pause();
-//             }
-//         }
-
-//         if keyboard_input.just_pressed(KeyCode::Up) {
-//             let speed = player.speed();
-//             player.set_speed(speed * 1.2);
-//         }
-
-//         if keyboard_input.just_pressed(KeyCode::Down) {
-//             let speed = player.speed();
-//             player.set_speed(speed * 0.8);
-//         }
-
-//         if keyboard_input.just_pressed(KeyCode::Left) {
-//             let elapsed = player.elapsed();
-//             player.set_elapsed(elapsed - 0.1);
-//         }
-
-//         if keyboard_input.just_pressed(KeyCode::Right) {
-//             let elapsed = player.elapsed();
-//             player.set_elapsed(elapsed + 0.1);
-//         }
-
-//         if keyboard_input.just_pressed(KeyCode::Return) {
-//             *current_animation = (*current_animation + 1) % animations.0.len();
-//             player
-//                 .play(animations.0[*current_animation].clone_weak())
-//                 .repeat();
-//         }
-//     }
-// }
-
-// ----------------------------------------------------------------
-//helmet example
 fn main() {
     App::new()
-        .insert_resource(AmbientLight {
-            color: Color::WHITE,
-            brightness: 1.0 / 5.0f32,
-        })
         .add_plugins(DefaultPlugins)
+        .add_plugin(BlenderPlugin)
         .add_startup_system(setup)
-        .add_system(animate_light_direction)
+        .add_system(camera::pan_orbit_camera)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(Camera3dBundle {
-        transform: Transform::from_xyz(0.7, 0.7, 1.0).looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
-        ..default()
-    });
-    const HALF_SIZE: f32 = 1.0;
-    commands.spawn_bundle(DirectionalLightBundle {
-        directional_light: DirectionalLight {
-            shadow_projection: OrthographicProjection {
-                left: -HALF_SIZE,
-                right: HALF_SIZE,
-                bottom: -HALF_SIZE,
-                top: HALF_SIZE,
-                near: -10.0 * HALF_SIZE,
-                far: 10.0 * HALF_SIZE,
-                ..default()
-            },
-            shadows_enabled: true,
-            ..default()
-        },
-        ..default()
-    });
-    commands.spawn_bundle(SceneBundle {
-        scene: asset_server.load("models/FlightHelmet/FlightHelmet.gltf#Scene0"),
-        ..default()
-    });
-}
+fn setup(mut commands: Commands, asset_server: ResMut<AssetServer>) {
+    // Create and spawn a Blender object using a BlenderObjectBundle
+    // This will only spawn the one object
+    // This example is included for completeness, but it is recommended to use spawn_blender_object instead
+    let mut suzanne = BlenderObjectBundle::new(&asset_server, "demo.blend", "Suzanne").unwrap();
+    suzanne.transform = Transform::from_translation(Vec3::new(-4.0, 0.0, 0.0));
+    commands.spawn_bundle(suzanne);
 
-fn animate_light_direction(
-    time: Res<Time>,
-    mut query: Query<&mut Transform, With<DirectionalLight>>,
-) {
-    for mut transform in &mut query {
-        transform.rotation = Quat::from_euler(
-            EulerRot::ZYX,
-            0.0,
-            time.seconds_since_startup() as f32 * std::f32::consts::TAU / 10.0,
-            -std::f32::consts::FRAC_PI_4,
-        );
-    }
-}
+    // Spawn Blender object with children
+    // The parent object's transform is taken from Blender
+    spawn_blender_object(
+        &mut commands,
+        &asset_server,
+        "demo.blend",
+        "TransformCube",
+        true,
+        None,
+    );
 
+    // Spawn Blender object with children
+    // The parent object's transform is provided
+    spawn_blender_object(
+        &mut commands,
+        &asset_server,
+        "demo.blend",
+        "Suzanne",
+        true,
+        Some(Transform::from_matrix(
+            Mat4::from_scale_rotation_translation(
+                Vec3::new(0.5, 0.5, 0.5),
+                Quat::IDENTITY,
+                Vec3::new(4.0, 0.0, 0.0),
+            ),
+        )),
+    );
+
+    // Light and camera
+    commands.spawn_bundle(PointLightBundle {
+        transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+        ..Default::default()
+    });
+
+    let translation = Vec3::new(5.0, 5.0, 5.0);
+    let radius = translation.length();
+
+    commands
+        .spawn_bundle(PerspectiveCameraBundle {
+            transform: Transform::from_translation(translation).looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        })
+        .insert(camera::PanOrbitCamera {
+            radius,
+            ..Default::default()
+        });
+}
