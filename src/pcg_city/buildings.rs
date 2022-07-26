cde749a4d2b995dafde4307b121ddd445dd30db0
parnikasaxena <paxsethorld@gmail.com>
2022-07-25 19:48:12 -0500

    pop up in grid

--- src/pcg_city/buildings.rs
+++ src/pcg_city/buildings.rs
@@ -1,12 +1,9 @@
 //uses bevy 0.7
 
-use crate::{
-    math::{city_perlin, grid, random},
-};
-use crate::{
-    camera::{dolly_free},
-};
-use bevy::{prelude::*};
+use crate::camera::dolly_free;
+use crate::math::random::get_block_location;
+use crate::math::{city_perlin, grid, random};
+use bevy::prelude::*;
 use bevy_dolly::prelude::*;
 use noise::{NoiseFn, Perlin, Seedable};
 use std::{thread, time};
@@ -24,60 +21,70 @@ pub fn spawn_buildings(
     )>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-
     //instantiate height noise function
     let height_noise_fn = city_perlin::HeightNoiseFn::default();
 
-   // if let Ok((mut transform, mut camera)) = query.p0().get_single_mut() {
+    // if let Ok((mut transform, mut camera)) = query.p0().get_single_mut() {
     if let Ok((mut rig)) = query.p1().get_single_mut() {
         let transform = rig.final_transform;
 
         let mut vector: Vec3 = transform.translation;
-        let x = vector.x;
-        let y = vector.y;
-        let z = vector.z;
-        println!("location: x = {}, y = {}, z = {}", x, y, z);        
-
-        // vector = transform.rotation.mul_vec3a(bevy::math::Vec3A::from((x, y, z))).into();
-        // println!("rotated vector: x = {}, y = {}, z = {}", vector.x, vector.y, vector.z);   
-
-        //vector += Vec3::new(0.0, 0.0, z + vector.z*5.0);
-        // if vector.z > 0.0 {
-        //     vector += Vec3::new(0.0, 0.0, z+5.0);
-        // }
-        // else{
-        //     vector += Vec3::new(0.0, 0.0, z-5.0); 
-        // }
-
-        vector = transform.rotation.mul_vec3a(bevy::math::Vec3A::from((vector.x, vector.y, vector.z))).into();
-        vector += Vec3::new(0.0, 0.0, z - 10.0);
-        //vector *= Vec3::new(0.5, 0.5, 0.5);
 
         let x_right_bound: i32 = convert(vector.x + 2.0);
-        let x_left_bound: i32 = convert(vector.x - 1.0);
+        let x_left_bound: i32 = convert(vector.x - 2.0);
 
-        for i in x_left_bound..x_right_bound {
-            let coord = grid::Coordinate {
-                x: i as f64,
-                y: vector.y as f64,
-                z: vector.z as f64,
-            };
-    
-            println!("{:?}", coord);
-    
-            let height = height_noise_fn.function.get([coord.x, coord.z]);
-            let dimension = random::Random {
-                number: (height + 1.0),
-            };
-    
-            // cube
-            let size: f32 = dimension.number as f32;
-            commands.spawn_bundle(PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::Cube { size: size })),
-                material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-                transform: Transform::from_xyz(coord.x as f32, 0.0 as f32, coord.z as f32),
-                ..default()
-            });
+        let z_front_bound: i32 = convert(vector.z + 2.0);
+        let z_back_bound: i32 = convert(vector.z - 2.0);
+        let mut count = 0;
+        for x_iter in x_left_bound..x_right_bound {
+            for z_iter in z_back_bound..z_front_bound {
+                count += 1;
+                let x: f32 = (x_iter as f32);
+                let z = (z_iter as f32);
+                let cam_vec = Vec2::new(vector.x, vector.z);
+                let pot_cube_vec = Vec2::new(x, z);
+                if cam_vec.distance(pot_cube_vec) <= 2.0 {
+                    let coord = grid::Coordinate {
+                        x: x as f64,
+                        y: 0.0 as f64,
+                        z: z as f64,
+                    };
+
+                    println!("{:?}", coord);
+
+                    let height = height_noise_fn.function.get([coord.x / 7.0, coord.z / 7.0]);
+                    let dimension = random::Random {
+                        number: (height + 0.5),
+                    };
+
+                    // cube
+                    let size: f32 = dimension.number as f32;
+                    println!("{:?}", size);
+
+                    let rand_shift_one = get_block_location() * 0.1;
+                    let rand_shift_two = get_block_location() * 0.1;
+                    println!("one: {:?} two: {:?} ", coord.x + rand_shift_one, coord.z + rand_shift_two);
+
+                    commands.spawn_bundle(PbrBundle {
+                        mesh: meshes.add(Mesh::from(shape::Cube { size: 0.60 })),
+                        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+                        transform: Transform::from_xyz((coord.x) as f32, 
+                                                        0.30 as f32, 
+                                                        (coord.z) as f32),
+                        ..default()
+                    });
+
+                    commands.spawn_bundle(PbrBundle {
+                        mesh: meshes.add(Mesh::from(shape::Cube { size: size })),
+                        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+                        transform: Transform::from_xyz((coord.x + rand_shift_one) as f32, 
+                                                    0.6 + size / 2.0 as f32, 
+                                                    (coord.z + rand_shift_two) as f32),
+                        ..default()
+                    });
+                }
+            }
         }
+        println!("count {}", count);
     }
 }
