621e6e74352461f56dbef4fd11eef8e2735f61df
parnikasaxena <paxsethorld@gmail.com>
2022-07-25 20:56:44 -0500

    no overlapping buildings
    be creative in building structure

--- src/pcg_city/buildings.rs
+++ src/pcg_city/buildings.rs
@@ -1,6 +1,7 @@
 //uses bevy 0.7
 
 use crate::camera::dolly_free;
+use crate::math::grid::MyGrid;
 use crate::math::random::{get_block_location, get_block_dimensions};
 use crate::math::{city_perlin, grid, random};
 use bevy::prelude::*;
@@ -14,6 +15,7 @@ fn convert(x: f32) -> i32 {
 
 pub fn spawn_buildings(
     mut commands: Commands,
+    mut grid: ResMut<MyGrid>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut query: ParamSet<(
         Query<(&mut Transform, With<dolly_free::MainCamera>)>,
@@ -25,10 +27,10 @@ pub fn spawn_buildings(
     let height_noise_fn = city_perlin::HeightNoiseFn::default();
 
     // if let Ok((mut transform, mut camera)) = query.p0().get_single_mut() {
-    if let Ok((mut rig)) = query.p1().get_single_mut() {
+    if let Ok(mut rig) = query.p1().get_single_mut() {
         let transform = rig.final_transform;
 
-        let mut vector: Vec3 = transform.translation;
+        let vector: Vec3 = transform.translation;
 
         let x_right_bound: i32 = convert(vector.x + 2.0);
         let x_left_bound: i32 = convert(vector.x - 2.0);
@@ -39,55 +41,61 @@ pub fn spawn_buildings(
         for x_iter in x_left_bound..x_right_bound {
             for z_iter in z_back_bound..z_front_bound {
                 count += 1;
-                let x: f32 = (x_iter as f32);
-                let z = (z_iter as f32);
+                let x: f32 = x_iter as f32;
+                let z = z_iter as f32;
                 let cam_vec = Vec2::new(vector.x, vector.z);
                 let pot_cube_vec = Vec2::new(x, z);
                 if cam_vec.distance(pot_cube_vec) <= 2.0 {
-                    let coord = grid::Coordinate {
-                        x: x as f64,
-                        y: 0.0 as f64,
-                        z: z as f64,
-                    };
+                    if let Some(b) = grid.coordinate_system.get_mut(&vec![x_iter, z_iter]){
+                        if *b == false {
+                            *b = true;
+                            let coord = grid::Coordinate {
+                                x: x as f64,
+                                y: 0.0 as f64,
+                                z: z as f64,
+                            };
+        
+                            println!("{:?}", coord);
+        
+                            let height = height_noise_fn.function.get([coord.x / 7.0, coord.z / 7.0]);
+                            let dimension = random::Random {
+                                number: height + 1.0,
+                            };
+        
+                            // cube
+                            let size: f32 = dimension.number as f32;
+                            println!("Size: {:?}", size);
+        
+                            commands.spawn_bundle(PbrBundle {
+                                mesh: meshes.add(Mesh::from(shape::Cube { size: 0.80 })),
+                                material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+                                transform: Transform::from_xyz((coord.x) as f32, 
+                                                                -0.3 as f32, 
+                                                                (coord.z) as f32),
+                                ..default()
+                            });
+        
+                            let mut height = 0.1;
+        
+                            //should shift parts within 1x1 square (not necessarily one building)
 
-                    println!("{:?}", coord);
+                            let mut part_height = (size / 2.0) as f32;
+                            while height < size{
+                                commands.spawn_bundle(PbrBundle {
+                                    mesh: meshes.add(Mesh::from(shape::Cube { size: part_height})),
+                                    material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+                                    transform: Transform::from_xyz(coord.x as f32, 
+                                                                height + part_height / 2.0 as f32, 
+                                                                coord.z as f32),
+                                    ..default()
+                                });
+        
+                                height += part_height;
 
-                    let height = height_noise_fn.function.get([coord.x / 7.0, coord.z / 7.0]);
-                    let dimension = random::Random {
-                        number: (height + 1.0) * 2.0,
-                    };
-
-                    // cube
-                    let size: f32 = dimension.number as f32;
-                    println!("Size: {:?}", size);
-
-                    commands.spawn_bundle(PbrBundle {
-                        mesh: meshes.add(Mesh::from(shape::Cube { size: 0.60 })),
-                        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-                        transform: Transform::from_xyz((coord.x) as f32, 
-                                                        -0.2 as f32, 
-                                                        (coord.z) as f32),
-                        ..default()
-                    });
-
-                    let mut height = 0.1;
-                    let rand_shift_one = get_block_location() / 8.0;
-                    let rand_shift_two = get_block_location() / 8.0;
-
-                    while height < size{
-                        let part_height = (get_block_dimensions() / 2.0) as f32;
-                        println!("part: {}", part_height);
-
-                        commands.spawn_bundle(PbrBundle {
-                            mesh: meshes.add(Mesh::from(shape::Cube { size: part_height})),
-                            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-                            transform: Transform::from_xyz(coord.x as f32, 
-                                                        height + part_height / 2.0 as f32, 
-                                                        coord.z as f32),
-                            ..default()
-                        });
-
-                        height += part_height;
+                                part_height = part_height / 2.0;
+                                println!("part: {}", part_height);
+                            }
+                        }
                     }
                 }
             }
