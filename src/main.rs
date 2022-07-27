07eae689035fc9d20c50b903086f4261e7e99a4d
parnikasaxena <paxsethorld@gmail.com>
2022-07-27 07:17:50 -0500

    reformatted to ex, worldid=0

--- src/main.rs
+++ src/main.rs
@@ -37,28 +37,6 @@ use players::{create, info, movement};
 //         .run();
 //   }
 
-fn start_app(player_num: usize) -> (usize, SocketAddr, SocketAddr) {
-    let bind_addr = if player_num == 0 {
-        "127.0.0.1:4001".parse().unwrap()
-    } else {
-        "127.0.0.1:4002".parse().unwrap()
-    };
-
-    let remote_addr = if player_num == 0 {
-        "127.0.0.1:4002".parse().unwrap()
-    } else {
-        "127.0.0.1:4001".parse().unwrap()
-    };
-
-    (player_num, bind_addr, remote_addr)
-
-    // commands.insert_resource(network_config::StartupNetworkConfig {
-    //     client: player_num,
-    //     bind: bind_addr,
-    //     remote: remote_addr,
-    // });
-}
-
 // fn main() {
 //     App::new()
 //         .add_startup_system(network_config::setup_game)
@@ -79,58 +57,82 @@ fn start_app(player_num: usize) -> (usize, SocketAddr, SocketAddr) {
 //         .run();
 // }
 
-fn main() {
-    let mut args = std::env::args();
-    let base = args.next().unwrap();
-    if let Some(player_num) = args.next() {
-        let mut app = App::new();
-        println!("created world id: {:?}", app.world.id());
+//---------------------------------------------------------------- down here before
+// fn start_app(player_num: usize) -> (usize, SocketAddr, SocketAddr) {
+//     let bind_addr = if player_num == 0 {
+//         "127.0.0.1:4001".parse().unwrap()
+//     } else {
+//         "127.0.0.1:4002".parse().unwrap()
+//     };
 
-        println!("many worlds pring created?");
-        let (player_num, bind_addr, remote_addr) = start_app(player_num.parse().unwrap());
+//     let remote_addr = if player_num == 0 {
+//         "127.0.0.1:4002".parse().unwrap()
+//     } else {
+//         "127.0.0.1:4001".parse().unwrap()
+//     };
 
-        app.add_startup_system(network_config::setup_game);
-        //app.add_startup_system(create::spawn_players.system());
-        println!("world if: {:?}", app.world.id());
+//     (player_num, bind_addr, remote_addr)
 
-        // let mut stage = SystemStage::new();
-        // stage.with_system(create::spawn_players);
+//     // commands.insert_resource(network_config::StartupNetworkConfig {
+//     //     client: player_num,
+//     //     bind: bind_addr,
+//     //     remote: remote_addr,
+//     // });
+// }
 
-        //println!("world if {}", stage.world_id);
-        //with_system
+// fn main() {
+//     let mut args = std::env::args();
+//     let base = args.next().unwrap();
+//     if let Some(player_num) = args.next() {
+//         let mut app = App::new();
+//         println!("created world id: {:?}", app.world.id());
 
-        let stage = SystemStage::single(create::spawn_players);
-        app
-            .add_startup_stage("game_setup", stage)
-            .add_plugins(DefaultPlugins)
-            .add_plugin(BackrollPlugin)
-            .register_rollback_input::<movement::PlayerInputFrame, _>(
-                movement::sample_input.system(), //need .system()
-            )
-            .register_rollback_component::<info::Player>()
-            .insert_resource(network_config::StartupNetworkConfig {
-                client: player_num,
-                bind: bind_addr,
-                remote: remote_addr,
-            })
-            .add_rollback_system(movement::player_movement);
-        
-        println!("world id again: {:?}", app.world.id());
-        app.run();
-    } else {
-        println!("in else");
-        let mut child_1 = std::process::Command::new(base.clone())
-            .args(&["0"])
-            .spawn()
-            .unwrap();
-        let mut child_2 = std::process::Command::new(base)
-            .args(&["1"])
-            .spawn()
-            .unwrap();
-        child_1.wait().unwrap();
-        child_2.wait().unwrap();
-    }
-}
+//         println!("many worlds pring created?");
+//         let (player_num, bind_addr, remote_addr) = start_app(player_num.parse().unwrap());
+
+//         app.add_startup_system(network_config::setup_game);
+//         //app.add_startup_system(create::spawn_players.system());
+//         println!("world if: {:?}", app.world.id());
+
+//         // let mut stage = SystemStage::new();
+//         // stage.with_system(create::spawn_players);
+
+//         //println!("world if {}", stage.world_id);
+//         //with_system
+
+//         let stage = SystemStage::single(create::spawn_players);
+//         app
+//             .add_startup_stage("game_setup", stage)
+//             .add_plugins(DefaultPlugins)
+//             .add_plugin(BackrollPlugin)
+//             .register_rollback_input::<movement::PlayerInputFrame, _>(
+//                 movement::sample_input.system(), //need .system()
+//             )
+//             .register_rollback_component::<info::Player>()
+//             .insert_resource(network_config::StartupNetworkConfig {
+//                 client: player_num,
+//                 bind: bind_addr,
+//                 remote: remote_addr,
+//             })
+//             .add_rollback_system(movement::player_movement);
+
+//         println!("world id again: {:?}", app.world.id());
+//         app.run();
+//     } else {
+//         println!("in else");
+//         let mut child_1 = std::process::Command::new(base.clone())
+//             .args(&["0"])
+//             .spawn()
+//             .unwrap();
+//         let mut child_2 = std::process::Command::new(base)
+//             .args(&["1"])
+//             .spawn()
+//             .unwrap();
+//         child_1.wait().unwrap();
+//         child_2.wait().unwrap();
+//     }
+// }
+//---------------------------------------------------------------- ^here before
 
 // fn main() {
 //    // env::set_var("RUST_BACKTRACE", "1");
@@ -165,3 +167,62 @@ fn main() {
 //     app
 //         .run();
 // }
+
+fn start_app(player_num: usize) {
+    let bind_addr = if player_num == 0 {
+        "127.0.0.1:4001".parse().unwrap()
+    } else {
+        "127.0.0.1:4002".parse().unwrap()
+    };
+
+    let remote_addr = if player_num == 0 {
+        "127.0.0.1:4002".parse().unwrap()
+    } else {
+        "127.0.0.1:4001".parse().unwrap()
+    };
+
+    let mut app = App::new();
+    println!("world if: {:?}", app.world.id());
+
+    app.add_startup_system(network_config::setup_game)
+        .add_startup_stage(
+            "game_setup",
+            SystemStage::single(create::spawn_players),
+        )
+        .add_plugins(DefaultPlugins)
+        .add_plugin(BackrollPlugin)
+        .register_rollback_input::<movement::PlayerInputFrame, _>(
+            movement::sample_input.system(), //need .system()
+        )
+        .register_rollback_component::<info::Player>()
+
+        .insert_resource(network_config::StartupNetworkConfig {
+            client: player_num,
+            bind: bind_addr,
+            remote: remote_addr,
+        })
+        .add_rollback_system(movement::player_movement);
+        
+    app.run();
+}
+
+fn main() {
+    let mut args = std::env::args();
+    let base = args.next().unwrap();
+    if let Some(player_num) = args.next() {
+        println!("play num: {}", player_num);
+        start_app(player_num.parse().unwrap());
+    } else {
+        println!("in else");
+        let mut child_1 = std::process::Command::new(base.clone())
+            .args(&["0"])
+            .spawn()
+            .unwrap();
+        let mut child_2 = std::process::Command::new(base)
+            .args(&["1"])
+            .spawn()
+            .unwrap();
+        child_1.wait().unwrap();
+        child_2.wait().unwrap();
+    }
+}
