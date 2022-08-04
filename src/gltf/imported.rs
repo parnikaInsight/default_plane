93e755d2c5377bc00eb180b542c2fc9f862a96a9
parnikasaxena <paxsethorld@gmail.com>
2022-08-04 10:27:11 -0500

    sketchfab works, not gltf translation

--- src/gltf/imported.rs
+++ src/gltf/imported.rs
@@ -4,14 +4,16 @@ pub fn create_sponza(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
 ) {
-    let player_handle = asset_server.load("kronos/glTF/Sponza.gltf#Scene0");
+    //let player_handle = asset_server.load("kronos/glTF/Sponza.gltf#Scene0");
+    //let player_handle = asset_server.load("sketchfab/phoenix_bird/scene.gltf#Scene0");
+    let player_handle = asset_server.load("sketchfab/grass/scene.gltf#Scene0");
     commands.spawn_bundle(SceneBundle {
             transform: Transform {
-                translation: Vec3::new(0.0, 0.0, 0.0),
+                translation: Vec3::new(-10.0, 0.0, 0.0),
                 ..default()
             },
             scene: player_handle.clone(),
             ..default()
         });
-    println!("sponza");
+    println!("bird");
 }
